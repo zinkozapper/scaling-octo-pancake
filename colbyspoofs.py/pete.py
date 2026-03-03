89a99355fbf3d9fb43b81068dff185d390ca4599
colbys706 <colbys2476@gmail.com>
2026-03-03 09:56:03 -0700

    colbyspoof Version 1. Includes spoof 1

---
+++ colbyspoofs.py/pete.py
@@ -0,0 +1,5 @@
+sWhoLeft = input("Pete and Repeat were sitting in a boat. A fish ate Pete. Who's left? ").strip().lower()
+while sWhoLeft == "repeat":
+    sWhoLeft = input("Pete and Repeat were sitting in a boat. A fish ate Pete. Who's left? ").strip().lower()
+
+print("Wrong!")
