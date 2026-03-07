0d6d6b1466aa89973ef7becdd7c49be5cc97d496
DuelMaster1000 <battleofduel@gmail.com>
2026-03-06 22:45:25 -0700

    Fixed logical error with menu selection loop and altered the print function for the wins and losses for each team in the list of dictionaries to print each team on a separate line instead of just the entire list all at once.

--- p2_functions_are_us.py
+++ p2_functions_are_us.py
@@ -10,7 +10,11 @@ def introduction():
     print(
         "\nThis program is designed to simulate a soccer season with randomly generated values for game results.\nYou will choose the number of games that will be played and who your team will square off against."
     )
-    name = input("\nPlease enter your name for use throughout the program: ").strip().title()
+    name = (
+        input("\nPlease enter your name for use throughout the program: ")
+        .strip()
+        .title()
+    )
     print(f"\nWelcome to the program, {name}. Let's get the season started.")
     return name
 
@@ -19,13 +23,16 @@ def introduction():
 def initial_menu():
     while True:
         print("\n---Menu Selection---\n")
-        print("1 - Add a Team\n2 - Delete a Team\n3 - Play a Game\n4 - Record Lookup\n5 - End Program\n")
+        print(
+            "1 - Add a Team\n2 - Delete a Team\n3 - Play a Game\n4 - Record Lookup\n5 - End Program\n"
+        )
         menuSelection = input("Please select a menu option: ")
         if (
             menuSelection == "1"
             or menuSelection == "2"
             or menuSelection == "3"
             or menuSelection == "4"
+            or menuSelection == "5"
         ):
             return int(menuSelection)
         else:
@@ -46,38 +53,51 @@ def print_teams(teams):
 def select_team(teams, team_type="home"):
     while True:
         print_teams(teams)
-        team = input(f"\nPlease enter the {team_type} team from the list of teams: ").strip().upper()
+        team = (
+            input(f"\nPlease enter the {team_type} team from the list of teams: ")
+            .strip()
+            .upper()
+        )
         for item in teams:
             if team == item["team"]:
                 return team
                 break
         print("\nTeam not found in system. Please select a valid team from the system.")
 
-#Function to display final record for a team. It receives selected Team's data and displays it.
-#I want to have a list of team name, teams won against, and teams lost against.
-#I need to add wins/losses to teams lost against.
-def display_team_scores(teams = [], games_played = 0):
+
+# Function to display final record for a team. It receives selected Team's data and displays it.
+# I want to have a list of team name, teams won against, and teams lost against.
+# I need to add wins/losses to teams lost against.
+def display_team_scores(teams=[], games_played=0):
     if games_played == 0:
         print("\nNo games have been played. Returning to menu.")
         return
     while True:
         print_teams(teams)
-        team = input("\nPlease enter the team you would like to view records for (or type 'exit' to exit): ").strip().upper()
+        team = (
+            input(
+                "\nPlease enter the team you would like to view records for (or type 'exit' to exit): "
+            )
+            .strip()
+            .upper()
+        )
         skip = False
         if team.lower() == "exit":
             break
         for item in teams:
             if team == item["team"]:
-                print(
-                    f"\n{team} has won against the following teams:"
-                    f"\n{item["won against"]}"
-                    "\nThey have lost against the following teams:"
-                    f"\n{item["lost against"]}"
-                )
+                print(f"\n{team} has won against the following teams:")
+                for the_team in item["won against"]:
+                    print(the_team)
+
+                print("\nThey have lost against the following teams:")
+                for the_team in item["lost against"]:
+                    print(the_team)
                 skip = True
                 break
         if skip == False:
             print("Invalid selection. Enter a valid team.")
+
 
 # Main function with function calls
 teams = []
@@ -93,7 +113,7 @@ while True:
             if teamAdd == team["team"]:
                 addIt = False
         if addIt == True:
-            teams.append({"team" : teamAdd, "won against" : [], "lost against" : []})
+            teams.append({"team": teamAdd, "won against": [], "lost against": []})
         else:
             print("\nTeam already entered. Returning to menu.")
     elif menuSelection == 2:
@@ -105,12 +125,20 @@ while True:
         else:
             while True:
                 skip = False
-                team_delete = input("\nPlease enter the name of the team you'd like to delete (or 'exit' to exit function): ").strip().upper()
+                team_delete = (
+                    input(
+                        "\nPlease enter the name of the team you'd like to delete (or 'exit' to exit function): "
+                    )
+                    .strip()
+                    .upper()
+                )
                 if team_delete.lower() == "exit":
                     break
                 for team in teams:
                     if team_delete == team["team"]:
-                        print(f"\nThe team {team_delete} has been removed from the system.")
+                        print(
+                            f"\nThe team {team_delete} has been removed from the system."
+                        )
                         teams.remove(team)
                         skip = True
                 if skip == False:
