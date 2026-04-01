ea0ab4c3414e57007b67f3715f8edfab3071f070
zinko <zinkogamez@gmail.com>
2026-04-01 15:41:35 -0600

    Updated packages needed for p4

---
+++ P4-ConferenceScraping/main.py
@@ -0,0 +1,8 @@
+# Author: Daniel Walker, Colby Seeley, Adam Halliday
+# Program to scrape general conference
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import sqlalchemy
+import matplotlib.pyplot as plt
